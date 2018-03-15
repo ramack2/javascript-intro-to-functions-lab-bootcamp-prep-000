0943018b2673e1b27946077dfe0a0fe4e0a2a421
Kriselle Ramac <krisellaella@gmail.com>
2018-03-15 17:25:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase())
-    return "I cant' hear you!"
+    return "I can't hear you!"
   if (string === string.toUpperCase())
     return "YES INDEED!"
 }
