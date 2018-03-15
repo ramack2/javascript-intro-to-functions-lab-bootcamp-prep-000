07fed587e9a72ad6d3aa38fcae40700d58051c28
Kriselle Ramac <krisellaella@gmail.com>
2018-03-15 17:33:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase)
+  var string = words
+  console.log(words.toUpperCase)
 }
 
 function logWhisper(string) {
